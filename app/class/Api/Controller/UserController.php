82e41622c273d8645d6b5ce007dc66ffdb4b3bfe
faaat <zjf580@163.com>
2020-08-14 15:43:32 +0800

    fix is vip

--- app/class/Api/Controller/UserController.php
+++ app/class/Api/Controller/UserController.php
@@ -17,13 +17,13 @@ Class UserController extends Controller {
         $sql = 'SELECT * FROM t_user WHERE device_id = ?';
         $userInfo = $this->locator->db->getRow($sql, $deviceId);
         if ($userInfo) {
-            return array('vipTime' => strtotime($userInfo['vip_time']) * 1000, 'userStatus' => $userInfo['user_status'], 'accessToken' => $userInfo['access_token']);
+            return array('isVip' => (strtotime($userInfo['vip_time']) > time()) ? 1 : 0, 'userStatus' => $userInfo['user_status'], 'accessToken' => $userInfo['access_token']);
         } else {
             $accessToken = md5($deviceId . time());
 
             $sql = 'INSERT INTO t_user (user_source, device_id, access_token, oaid, imei, mac, brand, model) SELECT :user_source, :device_id, :access_token, :oaid, :imei, :mac, :brand, :model FROM DUAL WHERE NOT EXISTS (SELECT user_id FROM t_user WHERE device_id = :device_id)';
             $this->locator->db->exec($sql, array('user_source' => $this->params('source'), 'device_id' => $deviceId, 'access_token' => $accessToken, 'oaid' => $this->params('oaid'), 'imei' => $this->params('imei'), 'mac' => $this->params('mac'), 'brand' => $this->params('brand'), 'model' => $this->params('model')));
-            return array('vipTime' => 0, 'userStatus' => 1, 'accessToken' => $accessToken);
+            return array('isVip' => 0, 'userStatus' => 1, 'accessToken' => $accessToken);
         }
     }
 }
