481855c2697bfacbe5dc567aa2ff701c6453e390
faaat <zjf580@163.com>
2020-08-17 17:30:04 +0800

    fix alipay callback

--- core/Core/Alipay.php
+++ core/Core/Alipay.php
@@ -19,7 +19,7 @@ class Alipay {
         $aop->gatewayUrl = 'https://openapi.alipay.com/gateway.do';
         $aop->appId = '2021001189607006';
         $aop->rsaPrivateKey = 'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCisMCHp4xJeQtUPG4qhN+yElWPmEx34ctohoibhitNE+7H+B6AeZpO3DIzw9YW1dKuItpsNMWmhn/5Lu21DrDOZh4VaBG6SBBW2Iyq3uxlS2x+MiE2j0NqyfPO/fLpDlwUr1FuPzOvHbE1ssvVM9iL+ZnJcVf/tLH9wXwzATERMqinOH5N/39603de2tr7wEANKkwemBs9ptHu0F7MFhLOTP8471rX4i1U2U3W6g/bZmYwy3Vs3Wzd3946Ra1ticpWDVDV2sflrGwpkgi0KqDpiD138UwrDQj7fF7WvjwJBW7gLBMXqLcSFcOPVwmLiMoT7I95gxZig12H3QUFWf1BAgMBAAECggEACciBO2ca64wo3z7nDQ2Cei3aEVGCP69HURjN/DQ8RF1PfZzxEJ6/ZcCeEDjVlffzvF8CLYGa5SGvbmehCcNBZJgFdRoV/tK4kNBi3R+crZa0hn4zOxmwXyqXy7m/sr4XUXMdfXi1ffFWJ7mBwmdkvT4cPl3fgdP25CCPfG206qi9fJYw9OzlqpzYZEu5e3/cS5VGLXKBh+SjK6n9B63Zh82bp/OihWGesj4Vi7JbSBFOM7xrxy8uODDDW8q/lBWFojaNDsccQIonoXpLmARAfABIU5L8Xvh7givU4MFVc4TwmG8YLbkVipMQZ1wMAouPXJBfvxmCApazmPzKDTbspQKBgQDmLvwRbwqf7teq89kT2BZl2tW3S8mvnqf2Bwd44Hpo3Dyv9T9z6BV9pLa7EiAICxN9Bysa+rG4WZ7rj5Y59CrCe2XQinbEJZScX/q8eJfK6dpVw0zRCxViUe4Wi63Eh4wfWeKpLq5dWFVhIYq97gJ/RBRGY4NMYtY5AfBBhGXXCwKBgQC07+jrNPh48d6hy9oaVgx43Enxmg2w5818l524iUTuXnhHG7gOXYEzLBblBRqlHWQbcAAiFoR6B7rnU/WU8DzyLOEZ9s3k+6IYscGUtKpiGNVQO4pOb77Ys9yvBNSxIhcFg1GfpG4WtgJyxVUSvQ6lOb5peCJnlLWCmkvswHv8YwKBgHvfyy2CqaAaRBwu8KK6Rot38k2bTqXhZxiC/eVyQM4Pv+UdwZEZ0/7y1pfkEDLj6w/8/JifU2cXa+vvMPRtT1msWMWazoGOi+R/zosBBwdfRG2lFcDmCxMHbm7ZqqE6JRF2KQHNKm73q7MC/wxpexSMSbD7utwv0IOLZIWNv9SzAoGAblhhlBAZ+KiJPeM0gBs6P/sYnV92Og0kJHfSmFge0cCLWdJtzVT5FlwtGj6ioU/rXVBQxHk3EbTlJ27stohMouT74vnBV4SetrCxfh8wSeMbNHMbRfqgSUhnrdUkYWKI57POc62z9eXKWHRADc1+wQUWOvwo/0KR77Rp2VkKREECgYAeZej/HA/UgVeX3D4MMqD7Qsopd/UNvqMysZTSqku7ZVmRmsaNU/fRwb6IYSNUA9tJa/F+AamsbfyAI6v9GD+uqQXvQHDrRDaoyZSC4X1DktCc96/W4GsRXLUQLjW7ltsp5FJtaHeELm2TgVrldduQe85tcUIf/q/KqGFtJBbTrw==';
-        $aop->alipayrsaPublicKey='MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAorDAh6eMSXkLVDxuKoTfshJVj5hMd+HLaIaIm4YrTRPux/gegHmaTtwyM8PWFtXSriLabDTFpoZ/+S7ttQ6wzmYeFWgRukgQVtiMqt7sZUtsfjIhNo9Dasnzzv3y6Q5cFK9Rbj8zrx2xNbLL1TPYi/mZyXFX/7Sx/cF8MwExETKopzh+Tf9/etN3Xtra+8BADSpMHpgbPabR7tBezBYSzkz/OO9a1+ItVNlN1uoP22ZmMMt1bN1s3d/eOkWtbYnKVg1Q1drH5axsKZIItCqg6Yg9d/FMKw0I+3xe1r48CQVu4CwTF6i3EhXDj1cJi4jKE+yPeYMWYoNdh90FBVn9QQIDAQAB';
+        $aop->alipayrsaPublicKey='MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjUzhXrdJ7GDsgJ59fMLlk7hyYrXkkeGwnYD/eO2HBZh39Y9gTfLJ61Yogc7keOn2uAnZY/zBlw3n+T6mb6/5JYFgvXQi8Qzeh6BkBrNnROu+k4PjhmSbORJFoLrrIxDnsYkQ995kYYhpbS0yf2Al++55v4SrD3/YoVBhWPcRg4xI0QD94FLwhCmcCkft/ILRtUxQk2QeVPLSesvMx2mmUK2L2x2hFA8ewRoGmUdG2Fu9YFIxk//16RI+H7KI8LaoXoVDqHobPae9p0ACE7k9G5vs/cYuikSMKu+lnxghte1jNO+CqrvTP4Pes/mW4e7CEMCTAmEnsXLUrQ6FpfKMcQIDAQAB';
         $aop->apiVersion = '1.0';
         $aop->signType = 'RSA2';
         $aop->postCharset='UTF-8';
@@ -177,8 +177,8 @@ class Alipay {
    }
 
    public function verify () {
-       $aop = new \AopClient ();
-       $aop->alipayrsaPublicKey='MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAorDAh6eMSXkLVDxuKoTfshJVj5hMd+HLaIaIm4YrTRPux/gegHmaTtwyM8PWFtXSriLabDTFpoZ/+S7ttQ6wzmYeFWgRukgQVtiMqt7sZUtsfjIhNo9Dasnzzv3y6Q5cFK9Rbj8zrx2xNbLL1TPYi/mZyXFX/7Sx/cF8MwExETKopzh+Tf9/etN3Xtra+8BADSpMHpgbPabR7tBezBYSzkz/OO9a1+ItVNlN1uoP22ZmMMt1bN1s3d/eOkWtbYnKVg1Q1drH5axsKZIItCqg6Yg9d/FMKw0I+3xe1r48CQVu4CwTF6i3EhXDj1cJi4jKE+yPeYMWYoNdh90FBVn9QQIDAQAB';
+//       $aop = new \AopClient ();
+//       $aop->alipayrsaPublicKey='MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjUzhXrdJ7GDsgJ59fMLlk7hyYrXkkeGwnYD/eO2HBZh39Y9gTfLJ61Yogc7keOn2uAnZY/zBlw3n+T6mb6/5JYFgvXQi8Qzeh6BkBrNnROu+k4PjhmSbORJFoLrrIxDnsYkQ995kYYhpbS0yf2Al++55v4SrD3/YoVBhWPcRg4xI0QD94FLwhCmcCkft/ILRtUxQk2QeVPLSesvMx2mmUK2L2x2hFA8ewRoGmUdG2Fu9YFIxk//16RI+H7KI8LaoXoVDqHobPae9p0ACE7k9G5vs/cYuikSMKu+lnxghte1jNO+CqrvTP4Pes/mW4e7CEMCTAmEnsXLUrQ6FpfKMcQIDAQAB';
        return $this->aop->rsaCheckV1($_POST, NULL, "RSA2");
    }
 }
